f00967d2ddfe90c7b34b0bc9c55affce20b84de0
cheng <y2984491@gmail.com>
2022-12-01 14:20:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+
+// everytime the orientation of the device is changed the web would reload,
+window.onorientationchange = function(){
+    window.location.reload()
+}
+
+//show navbar
+
+$(".menu").click(function(){
+    $(".mobile-sidebar").css('display',"flex");
+});
+
+
+//hidenavba
+
+$(".close-icon").click(function(){
+    $(".mobile-sidebar").hide()
+})
+
+$('.nav-links').children().click(function(){
+    $(".mobile-sidebar").hide()
+})
+
+
+})
